b72a25db9ca1bbd664688912fa780aab936337a3
Carmine <Carmineporricelli96@gmail.com>
2021-09-09 12:26:25 +0200

    Embed Youtube nascosto per l'autoplay della canzone

--- components/Player.tsx
+++ components/Player.tsx
@@ -1,12 +1,21 @@
-import React, { useEffect } from "react";
+import React from "react";
 
 function Player() {
-  useEffect(() => {
-    const audio = new Audio("/per_dimenticare.mp3");
-    audio.loop = true;
-    audio.play();
-  }, []);
-  return <></>;
+  return (
+    <>
+      <iframe
+        className="invisible"
+        loading="lazy"
+        width="0px"
+        height="0px"
+        src={`https://www.youtube-nocookie.com/embed/6I_gY_fyY_8?autoplay=1`}
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+        title="Embedded youtube"
+      />
+    </>
+  );
 }
 
 export default Player;
