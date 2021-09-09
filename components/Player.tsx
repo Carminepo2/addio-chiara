323bb7ee6076e127ffce7fa960295bd4320be797
Carmine <Carmineporricelli96@gmail.com>
2021-09-09 12:32:24 +0200

    trying with reactplayer

--- components/Player.tsx
+++ components/Player.tsx
@@ -1,19 +1,10 @@
 import React from "react";
+import ReactPlayer from "react-player";
 
 function Player() {
   return (
     <>
-      <iframe
-        className="invisible"
-        loading="lazy"
-        width="0px"
-        height="0px"
-        src={`https://www.youtube-nocookie.com/embed/6I_gY_fyY_8?autoplay=1`}
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-        title="Embedded youtube"
-      />
+      <ReactPlayer playing loop width={0} height={0} url={`https://www.youtube-nocookie.com/embed/6I_gY_fyY_8`} />
     </>
   );
 }
